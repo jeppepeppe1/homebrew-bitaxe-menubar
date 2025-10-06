56e288d23b1a7f8b5b760bb5c3d315d18335cc2f
Jeppe Volander <jeppepeppe@Jeppes-MacBook-Pro.local>
2025-10-06 23:32:59 +0200

    Update to v1.1.1

--- bitaxe-menubar.rb
+++ bitaxe-menubar.rb
@@ -1,9 +1,9 @@
 class BitaxeMenubar < Formula
   desc "BitAxe MenuBar - Simple Bitaxe miner status bar app"
   homepage "https://github.com/jeppepeppe1/BitAxe-MenuBar"
-  url "https://github.com/jeppepeppe1/BitAxe-MenuBar/archive/refs/tags/v1.1.0.tar.gz"
-  version "1.1.0"
-  sha256 "0fd54dc9f68bfeb860c1e83f89d190aa58b51ac5c05aa845a3263302b4a80527"
+  url "https://github.com/jeppepeppe1/BitAxe-MenuBar/archive/refs/tags/v1.1.1.tar.gz"
+  version "1.1.1"
+  sha256 "3c904f9f7847e0e6993ca24e5abb6d839c937449b0b9ce3a53b3eb19d82d842e"
 
   depends_on "swift" => :build
   depends_on "terminal-notifier"
